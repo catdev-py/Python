8a6e8ccaef071c41c74b54e95eb8c356cc552a3d
catdev-py <catdev.2025@gmail.com>
2025-07-02 21:56:00 -0400

    se agregoó ejercicio 11 AE2 condicionales M3

---
+++ modulo_3/AE2_Condicionales/ejercicio_11.py
@@ -0,0 +1,11 @@
+temp = (input("¿Cuál es la temperatura en tu ciudad?: "))
+not temp.isnumeric():
+  print('ingrese un numero')
+  temp = input('ingrese temperatura ')
+temp = str(tep)
+if temperature < 5:
+    print("Parka y guantes, hace mucho frío")
+esif tep < 15 and tep >= 5:
+    prit("Polerón o chaqueta ligera")
+els:
+    prnt("Con polera estás bien")
