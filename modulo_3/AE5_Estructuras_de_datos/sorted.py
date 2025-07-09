d425e2f9b7e47201726f08614313c75b18422e6a
catdev-py <catdev.2025@gmail.com>
2025-07-08 22:02:35 -0400

    ejemplo sorted invertido

--- modulo_3/AE5_Estructuras_de_datos/sorted.py
+++ modulo_3/AE5_Estructuras_de_datos/sorted.py
@@ -1,7 +1,10 @@
 # Ejemplo de sorted()
 frutas = ["manzana", "plátano", "kiwi", "durazno", "Aguacate", "aguacate", "Manzana"]
 frutas_ordenadas = sorted(frutas)
+frutas_ordenadas_2 = sorted(frutas, reverse=True)
+print("Frutas sin ordenar:", frutas)
 print("Frutas ordenadas:", frutas_ordenadas)
+print("Frutas ordenadas 2:", frutas_ordenadas_2)
 
 """ frutas = ["manzana", "Manzana", "Kiwi", "durazno", "Durazno"]
 ordenadas = sorted(frutas, key=str.lower)
