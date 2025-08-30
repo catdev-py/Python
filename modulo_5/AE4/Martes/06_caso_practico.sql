71a814e0135d0aeee0d53ddb414ba5949e40c473
catdev-py <catdev.2025@gmail.com>
2025-08-29 21:43:14 -0400

    se agregaron las presentaciones del jueve sy viernes 238 y 29 de agosto correspondintes al AE5 del M5

--- modulo_5/AE4/Martes/06_caso_practico.sql
+++ modulo_5/AE4/Martes/06_caso_practico.sql
@@ -23,3 +23,46 @@ CREATE TABLE animales (
 INSERT INTO duenos (nombre, telefono) VALUES ('Juan Pérez', '987654321');
 INSERT INTO animales (nombre, especie, edad, peso, dueno_id)
 VALUES ('Firulais', 'Perro', 3, 12.5, 1);
+
+-- Consultas
+SELECT * FROM duenos;
+SELECT * FROM animales;
+
+SELECT duenos.id, duenos.nombre, duenos.telefono, animales.id, animales.nombre, animales.especie, animales.edad, animales.peso
+FROM duenos
+INNER JOIN animales ON duenos.id = animales.dueno_id;
+
+-- Transactions
+START TRANSACTION;
+INSERT INTO duenos (nombre, telefono) VALUES ('Maria Gonzales', '987654321');
+INSERT INTO animales (nombre, especie, edad, peso, dueno_id)
+VALUES ('Firulais', 'Perro', 3, 12.5, 2);
+COMMIT;
+
+START TRANSACTION;
+INSERT INTO duenos (nombre, telefono) VALUES ('Maria Gonzales', '987654321');
+INSERT INTO animales (nombre, especie, edad, peso, dueno_id)
+VALUES ('Firulais', 'Perro', 3, 12.5, 2);
+ROLLBACK;
+
+-- Transaction with if
+START TRANSACTION;
+INSERT INTO duenos (nombre, telefono) VALUES ('Maria Gonzales', '987654321');
+INSERT INTO animales (nombre, especie, edad, peso, dueno_id)
+VALUES ('Firulais', 'Perro', 3, 12.5, 2);
+IF ROW_COUNT() > 0 THEN
+    COMMIT;
+ELSE
+    ROLLBACK;
+END IF;
+
+-- Transaction if else commit rollback
+START TRANSACTION;
+INSERT INTO duenos (nombre, telefono) VALUES ('Maria Gonzales', '987654321');
+INSERT INTO animales (nombre, especie, edad, peso, dueno_id)
+VALUES ('Firulais', 'Perro', 3, 12.5, 2);
+IF edad > 5 THEN
+    COMMIT;
+ELSE
+    ROLLBACK;
+END IF;
